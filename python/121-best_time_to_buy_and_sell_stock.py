b1744802c42c5c6da5bfd1e2a25c5efaaa0549d6
zhangang <zhangang1993@gmail.com>
2016-01-19 23:41:57 +0800

    ac 121

---
+++ python/121-best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,20 @@
+# 题目是求累和最高的一段。该方法求每一个数(第二个数起)作为最大值结尾得到的结果，在所有结果中选最大值即为最终结果。
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        array_len = len(prices)
+        if array_len<2:
+            return 0
+        d_min = prices[0]
+        result = 0
+        for i in range(1, array_len):
+            gap = prices[i] - d_min
+            if gap > result:
+                result = gap
+            if prices[i] < d_min:
+                d_min = prices[i]
+        return result
